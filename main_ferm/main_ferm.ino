17a5f8f063151f873b779c05630a4dac23499d41
Ezequiel Flores <ezequiel.flores@cokiba.org.ar>
2017-11-27 14:36:46 -0300

    corte de emergencia para dejar descansar el banco de frio

--- main_ferm/main_ferm.ino
+++ main_ferm/main_ferm.ino
@@ -18,11 +18,20 @@ OneWire oneWire2(BUSONEWIRE2);
 DallasTemperature sensor2(&oneWire2);
 
 
-int tempSet;
+int tempSet1;
+int tempSet2;
+int seguridad1;
+int seguridad2;
+int parametroseguro;
 
 void setup() {  
 tempSet1=16;
 tempSet2=16;
+seguridad1=0;
+seguridad2 = 0;
+
+parametroseguro= 4; 
+
 Serial.begin(9600); 
 
 pinMode(RELAY1,OUTPUT);
@@ -54,31 +63,45 @@ void loop() {
 	sensor2.requestTemperatures();
 	float temp2=sensor2.getTempCByIndex(0);
 	
-	if(temp1 > tempSet1+HISTERESIS){
-	  digitalWrite(RELAY1,LOW);
-	  Serial.print("Ferm1 ON  ");
-	  Serial.print(temp1);
-	}
-	else{
-		if ( temp1<tempSet1-HISTERESIS){ 
-		  digitalWrite(RELAY1,HIGH); 
-		  Serial.print("Ferm1 OFF  ");
+	if ( seguridad1 < parametroseguro){
+		if(temp1 > tempSet1+HISTERESIS){
+		  digitalWrite(RELAY1,LOW);
+		  Serial.print("Ferm1 ON  ");
 		  Serial.print(temp1);
-		  }
-	}
-
-	if(temp2 > tempSet2+HISTERESIS){
-	  digitalWrite(RELAY2,LOW);
-	  Serial.print(" Ferm2 ON: ");
-	  Serial.println(temp2);
-	}
-	else{
-		if ( temp2<tempSet2-HISTERESIS){ 
-		  digitalWrite(RELAY2,HIGH); 
-		  Serial.print(" Ferm2 OFF: ");
-		  Serial.println(temp2);
-		  }
-	} 
+		  seguridad1 = seguridad1 + 1;
+		}
+		else{
+			if ( temp1<tempSet1-HISTERESIS){ 
+			  digitalWrite(RELAY1,HIGH); 
+			  Serial.print("Ferm1 OFF  ");
+			  Serial.print(temp1);		  
+			  }
+		}
+	}else{
+	digitalWrite(RELAY1,HIGH); 
+	Serial.print("Ferm1 Parada de Emergencia:  ");
+	Serial.print(temp1);		
+	seguridad1 = 0;
+	}	
 	
+	if ( seguridad2 < parametroseguro){
+		if(temp2 > tempSet2+HISTERESIS){
+		  digitalWrite(RELAY2,LOW);
+		  Serial.print(" Ferm2 ON: ");
+		  Serial.println(temp2);
+		}
+		else{
+			if ( temp2<tempSet2-HISTERESIS){ 
+			  digitalWrite(RELAY2,HIGH); 
+			  Serial.print(" Ferm2 OFF: ");
+			  Serial.println(temp2);
+			  }
+		}
+	}else{
+	digitalWrite(RELAY2,HIGH); 
+	Serial.print("Ferm2 Parada de Emergencia:  ");
+	Serial.print(temp2);		
+	seguridad2 = 0;
+	}	
   }
 }
