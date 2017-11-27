9240f3bfecc8dd14f9dcff984ad40b15379cfe64
ezequiel Flores <ezequielsebastianflores@gmail.com>
2017-11-27 14:17:55 -0300

    checkpoint

--- main_ferm/main_ferm.ino
+++ main_ferm/main_ferm.ino
@@ -5,7 +5,7 @@
 #include <DallasTemperature.h>
 
 #define RELAY1 2
-#define HISTERESIS 0.6
+#define HISTERESIS 0.3
 #define BUSONEWIRE 9
 
 Tempo t_temp(30*1000); // temporizador para la lectura de temperatura
@@ -15,9 +15,15 @@ DallasTemperature sensors(&oneWire);
 int tempSet;
 
 void setup() {  
-  tempSet=14;
+  tempSet=16;
   Serial.begin(9600); 
-  pinMode(RELAY1,OUTPUT);   
+  pinMode(RELAY1,OUTPUT);
+digitalWrite(RELAY1,HIGH);
+sensors.requestTemperatures();
+float temp1=sensors.getTempCByIndex(0); 
+Serial.print("OFF  ");
+Serial.println(temp1); 
+
 }
 
 void loop() {
@@ -27,12 +33,13 @@ void loop() {
         float temp1=sensors.getTempCByIndex(0);
         
         if(temp1 > tempSet+HISTERESIS){
-          digitalWrite(RELAY1,HIGH);
+          digitalWrite(RELAY1,LOW);
           Serial.print("ON  ");
           Serial.println(temp1);
       }
       else{
-        if ( temp1<tempSet-HISTERESIS){  
+        if ( temp1<tempSet-HISTERESIS){ 
+          digitalWrite(RELAY1,HIGH); 
           Serial.print("OFF  ");
           Serial.println(temp1);
           }
